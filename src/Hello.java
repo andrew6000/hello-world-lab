48fb724b3d3ea962ebf1df8cb7ab11720a84cc5c
andrew6000 <rynkfamily33496@gmail.com>
2019-12-10 23:39:14 -0500

    Added hello world code

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Hello {
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+        String name;
+        String age;
+        String thePlaceWhereYouLive;
+        System.out.print("Hello. What is your name? ");
+        name = scanner.next();
+        System.out.print("It's nice to meet you, " + name + ". How old are you? ");
+        age = scanner.next();
+        System.out.println("I see that you are still quite young at only " + age + ".");
+        System.out.print("Where do you live? ");
+        thePlaceWhereYouLive = scanner.next();
+        System.out.print("Wow! I've always wanted to go to " + thePlaceWhereYouLive + ". Thanks for chatting with me. Bye!");
+
+    }
+}
